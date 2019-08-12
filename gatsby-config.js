f599f98ea7c124a0f23b2498e41dc581498aa26e
Will Rainbow <will@muzemaster.com>
2019-08-12 11:05:06 -0400

    Added Google Analytics plugin and tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,32 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-145484441-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "UA-145484441-1",
+        // Enables Google Optimize Experiment ID
+        experimentId: "UA-145484441-1D",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "thecontentcycle.com",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
